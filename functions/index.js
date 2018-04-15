e0f05f8ed700b1f57f2f41d20076f78c58ee9f3a
Ibe Arua <stanarua@aol.com>
2018-04-15 17:01:38 +0100

    push with correction on firebase function

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,16 @@ const nodemailer = require('nodemailer');
 const gmailEmail = encodeURIComponent(functions.config().gmail.email);
 const gmailPassword = encodeURIComponent(functions.config().gmail.password);
 const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
-exports.sendContactMessage = functions.database.ref('/messages/{pushKey}').onWrite(event => {
+
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+});
+
+exports.sendContactMessage = functions.database.ref('/participants/{pushKey}').onWrite(event => {
   const snapshot = event.data;
 // Only send email for new messages.
   if (snapshot.previous.val() || !snapshot.val().name) {
